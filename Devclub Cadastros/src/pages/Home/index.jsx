40ad90949f5e79d38b0df99828a4eac91efb08d8
misaiasx <mizaiascorrea@gmail.com>
2025-01-06 09:05:23 -0300

    add commit

---
+++ Devclub Cadastros/src/pages/Home/index.jsx
@@ -0,0 +1,69 @@
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import api from '../../services/api'
+
+import { Container, ContainerInputs, Form, Input, InputLabel, Title } from './styles'
+
+
+import Button from '../../components/Button'
+import TopBackground from '../../components/TopBackground'
+
+function Home() {
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  const navigate = useNavigate()
+
+
+
+  async function registerNewUser() {
+    await api.post("/usuarios", {
+      email: inputEmail.current.value,
+      age: parseInt(inputAge.current.value),
+      name: inputName.current.value,
+    })
+
+  }
+
+
+  return (
+    <Container>
+      <TopBackground />
+
+      <Title>Cadastrar Usuários</Title>
+
+      <Form>
+        <ContainerInputs>
+
+          <div>
+            <InputLabel>
+              Nome<span> *</span></InputLabel>
+            <Input type='text' placeholder='Nome do usuário' ref={inputName} />
+          </div>
+          <div>
+            <InputLabel>
+              Idade<span> *</span>
+            </InputLabel>
+            <Input type='number' placeholder='Idade de usuário' ref={inputAge} />
+          </div>
+
+        </ContainerInputs>
+        <div style={{ width: '100%' }}>
+          <InputLabel>
+            Email<span> *</span>
+            <Input type='email' placeholder='E-mail do usuário' ref={inputEmail} />
+          </InputLabel>
+        </div>
+
+
+        <Button type='button' onClick={registerNewUser} theme="primary">Cadastrar Usuário</Button>
+      </Form>
+      <Button type='button' onClick={() => navigate('/lista-de-usuarios')} >Ver lista de  Usuários</Button>
+    </Container>
+
+
+  )
+}
+
+export default Home
